372ddd8dc7a0e18d9687027d54a49807a5cdefce
Fahad Brohi <fahad.shah123.fsb@gmail.com>
2020-08-08 19:59:16 +0500

    quizapp

---
+++ build/precache-manifest.bef8cfe1187947fb86d96c6c1e0d45e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e94ff88e067e9bc7d25b2b013f9c462",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffd2313a2f264a842799",
+    "url": "/static/css/main.039625f1.chunk.css"
+  },
+  {
+    "revision": "9acd4747b949810ec9fc",
+    "url": "/static/js/2.071d72a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.071d72a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffd2313a2f264a842799",
+    "url": "/static/js/main.e19b8ff2.chunk.js"
+  },
+  {
+    "revision": "3c7beecf55bdb387b928",
+    "url": "/static/js/runtime-main.d3f8a8db.js"
+  }
+]);
